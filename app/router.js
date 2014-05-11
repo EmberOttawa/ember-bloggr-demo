04ce1cd60e36b181f88e1bf9383f9369363d7b1c
Owain Williams <owain@Owains-MacBook-Air-7.local>
2014-05-11 17:41:56 -0400

    Hook up navigation
    - Specify routes in ``router.js``
    - Add base application markup (navigation header) to ``application.hbs``
    - add an ``index.hbs`` to render into default application ``{{outlet}}``
    - add ``about.hbs`` to render on static about page

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 var Router = Ember.Router.extend();
 
-// Router.map(function() {
-//   this.route('about');							//  /about
-//   this.resource('posts', function() {			//  /posts
-//     this.route('post', { path: ':post_id' });	//  /posts/:post_id
-//   });
-// });
+Router.map(function() {
+  this.route('about');							//  /about
+  this.resource('posts', function() {			//  /posts
+    this.route('post', { path: ':post_id' });	//  /posts/:post_id
+  });
+});
 
 export default Router;
