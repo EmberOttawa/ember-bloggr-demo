3f8b66771fc6f5cea8fa459bd6023160aa11b5ca
Owain Williams <owain@Owains-MacBook-Air-7.local>
2014-05-11 17:46:52 -0400

    Hook up Post detail page & editing
    - Adds ``post.hbs`` to render post data
    - back the post template with a controller to allow for editing

---
+++ app/controllers/posts/post.js
@@ -0,0 +1,13 @@
+export default Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
